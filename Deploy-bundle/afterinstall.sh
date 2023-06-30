9ecd6a905c1b8e5326aeb2dd392e92a228323673
vinnu467 <134151077+vinnu467@users.noreply.github.com>
2023-06-30 15:07:20 +0530

    Update afterinstall.sh

--- Deploy-bundle/afterinstall.sh
+++ Deploy-bundle/afterinstall.sh
@@ -12,5 +12,6 @@ LATEST_OBJECT=$(aws s3api list-objects-v2 --bucket "$S3_BUCKET" --query 'sort_by
 
 # Copy the latest WAR file from S3 to webapps directory
 aws s3 cp "s3://$S3_BUCKET/$LATEST_OBJECT" "$TOMCAT_WEBAPPS_DIR"
+mv *.war vprofile-app.war
 
 echo "artifact copied succesfully"
